1cff33401f0b05dc2d4f772f841d103cb58a4164
Olesya Vetoshkina <our_post-office@mail.ru>
2022-11-02 08:30:53 +0300

    third commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,48 +1,46 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
 
-  let d = b ** 2 - 4 * a * c;
+  const differential = b ** 2 - 4 * a * c;
+  const root = -b / (2 * a);
+  const rootFirst = (-b + Math.sqrt(differential) ) / (2 * a);
+  const rootSecond = (-b - Math.sqrt(differential) ) / (2 * a);
   
-  if(d < 0) {
-    arr = [];
-  } else if(d === 0) {
-    arr = [-b/(2*a)];
-  } else if(d > 0) {
-    arr = [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
-}
+  if(differential === 0) {
+    arr.push(root);
+  } else if(differential > 0) {
+    arr.push(rootFirst, rootSecond);
+  }
 
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  const textErrorPercent = `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+  const textErrorContribution = `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
+  const textErrorAmount = `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+
   
-  if(Boolean(Number(percent)) === false && percent != 0) {
-      alert('Параметр "Процентная ставка" содержит неправильное значение ' + percent);
-    } 
-  percent = Number(percent);
-
-  if(Boolean(Number(contribution)) === false && contribution != 0) {
-      alert('Параметр "Начальный взнос" содержит неправильное значение ' + contribution);
-    } 
-  contribution = Number(contribution);
-
-  if(Boolean(Number(amount)) === false && amount != 0) {
-      alert('Параметр "Общая стоимость" содержит неправильное значение ' + amount);
-    } 
-  amount = Number(amount);    
+  if(isNaN(percent) === true) {
+    return textErrorPercent;
+  } else if(isNaN(contribution) === true) {
+    return textErrorContribution;
+  } else if(isNaN(amount) === true) {
+    return textErrorAmount;
+  }
 
   let s = amount - contribution;
 
   let today = new Date();
   let timeDiff = Math.abs(date.getTime() - today.getTime());
-  let n = Math.ceil(timeDiff / (1000 * 60 * 60 * 24 * 30));  
+  let n = Math.ceil(timeDiff / (1000 * 60 * 60 * 24 * 30.5));  
    
   let p = percent / 12 / 100;
   let payment = s * (p + (p / (((1 + p) ** n) - 1)));
 
-  totalAmount = (payment * n).toFixed(2);   
+  totalAmount = +(payment * n).toFixed(2);   
 
   return totalAmount;
 }
